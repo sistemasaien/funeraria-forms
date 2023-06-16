b896d2bde79aecf7c8e6c34b0d2a16da02fbcc53
sistemasaien <127794947+sistemasaien@users.noreply.github.com>
2023-06-16 09:40:02 -0300

    Create database.js

---
+++ src/controllers/database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.MYSQLDATABASE,
+    keepAlive: true
+});
+
+//getConnection
+connection = pool.getConnection(function (err, connection) {
+    if (err) {
+        connection.release();
+        console.error('Error al conectarse a la base de datos:', err);
+        setTimeout(connect, 2000); // intenta reconectar después de 2 segundos
+    } else {
+        console.log('Conexión establecida con la base de datos');
+    }
+});
+
+module.exports = pool;
